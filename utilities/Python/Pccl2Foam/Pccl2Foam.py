9113fccba1cad307fad6af5bb45e34b7162522d6
Clint Dunn <cdunn6754@gmail.com>
2018-05-15 09:26:31 -0600

    Applies another fix to the calculation of volatile gas fractions in Pccl2Foam
    I made a mistake earlier in the way I calculated the fractions. This time I
    checked that converting the outputs intended for OF, which are not in daf,
    back into daf reproduced the original PCCL outputs, which are in daf.
    This gives me greater confidence in this method.

--- utilities/Python/Pccl2Foam/Pccl2Foam.py
+++ utilities/Python/Pccl2Foam/Pccl2Foam.py
@@ -1,11 +1,11 @@
 # Convert some of the PCCL outputs to usable inputs in OpenFOAM cloud properties files.
 
 ### Inputs from proximate and ultimate analysis (not PCCL)
-Y_ash = .0845
-Y_liq = 0.0758
+Y_ash = 0.0586
+Y_liquid = 0.1691
 # assume the liquid is water, then find 
 # fraction that is daf
-Y_daf = 1 - Y_ash - Y_liq
+Y_daf = 1.0 - Y_ash - Y_liquid
 
 ### Inputs from PCCL all are given on daf basis
 # Relevant File name: "FDC1WT1.RPT"
@@ -18,72 +18,90 @@ Y_gas_daf = 0.598 #NOTE: sum both Ygas and Ytar from file
 # Relevant File names:
 #   "FDC1WT1.RPT","FDC1HC1.RPT", "FDC1NG1.RPT"
 Y_i_daf = {
-    "TAR":0.417,
-    "CO2":0.027,
-    "H2O":0.055,
-    "CO":0.027,
-    "H2":0.0091,
-    "CH4":0.028,
-    "C2H2":0.0105,
-    "C2H6":0.0103
+    "TAR":0.357,
+    "CO2":0.043,
+    "H2O":0.068,
+    "CO":0.023,
+    "H2":0.0001,
+    "CH4":0.035,
+    "C2H2":0.00,
+    "C2H6":0.0021
 }
 
 ## Secondary Tar Breakdown
 # Relevant File Name: "SFTRC1T1.RPT"
 sec_species = {
-    "SOOT":.602,
-    "C2H2":.419e-1,
-    "H2":.524e-1,
-    "CO":.969e-1,
-    "H2O":0.137e-2,
-    "CO2":0.119e-1
+    "SOOT":.748,
+    "C2H2":.322e-1,
+    "H2":.470e-1,
+    "CO":.146,
+    "H2O":.566e-3,
+    "CO2":.365e-2
 }
 
 ### **** END INPUTS **** ###
 
 
+
 ### Convert Fractions to a non-daf basis
 ## Phase Fractions
-Y_char = Y_char_daf * Y_daf
-Y_solid = Y_char + Y_ash
-Y_gas = 1.0 - Y_solid - Y_liq # includes tar too
+# Y_char = Y_char_daf * Y_daf
+# Y_solid = Y_char + Y_ash
+# Y_gas = 1.0 - Y_solid - Y_liq # includes tar too
 
-## Solid Phase Fractions 
+Y_gas = Y_gas_daf * Y_daf # includes tar too
+Y_char = 1.0 - Y_gas - Y_liquid - Y_ash
+Y_solid = Y_char + Y_ash
+
+
+## Solid Phase Fractions (fraction of species within phase)
 ## (i.e. fraction of specie within the solid phase as in OpenFOAM dict)
 Y_char_solid = Y_char/Y_solid
 Y_ash_solid = Y_ash/Y_solid
 
-## Liquid Phase Fractions (for this all liquid is water)
+## Liquid Phase Fractions (fraction of species within phase)
 Y_water_liq = 1.0
 
-## Gas Phase Fractions 
+## Gas Phase Fractions (fraction of specie within phase)
 # here we make the assumption that Y_i_daf/Y_gas_daf = Y_i/Y_gas
 # which I think is pretty much true.
 # Note that tar fraction is preserved and the rest are normalized to 
 # account for missing species.
-gas_sum_unnormalized = sum([Y_i_daf[name] for name in Y_i_daf.keys() if name != "TAR"])
-Y_gas_no_tar = Y_gas - Y_i_daf["TAR"]
+
+gas_sum_no_tar = sum([Y_i_daf[name] for name in Y_i_daf.keys() if name != "TAR"])
+
 for species in Y_i_daf.keys():
     if species == "TAR":
         continue
     old_mf = Y_i_daf[species]
-    Y_i_daf[species] = (old_mf/gas_sum_unnormalized) * Y_gas_no_tar
+    Y_i_daf[species] = ( (old_mf/gas_sum_no_tar)  
+                         * (Y_gas_daf - Y_i_daf["TAR"]) )
+
+# Species fractions of gas, not daf
+Y_i_gas = {}
 
 # Now we need to normalize the entire array to total 1.0
-# i.e. currently sum(Y_i_daf.values()) == Y_gas, but we want
-# sum(Y_i_daf.values()) == 1.0
+# i.e. currently sum(Y_i_daf.values()) == Y_gas_daf, but we want
+# sum(Y_i.values()) == 1.0
 for species in Y_i_daf:
-    Y_i_daf[species] =Y_i_daf[species] / Y_gas
+    Y_i_gas[species] =Y_i_daf[species] / Y_gas_daf
+
+### END Analysis
     
+
+
+
+### Report the mass fraction that is DAF
+print("\nDAF mass fraction: \n Y_daf: {}".format(Y_daf))
 ### Report the phase fractions (gas, liquid, solid) (in OpenFOAM ready form)
-print("\nPhase Fractions:\n Gas: {:.3f}\n Liquid: {:.3f}\n Solid: {:.3f}\n".format(
-    Y_gas, Y_liq, Y_solid))
+print("\nPhase Fractions:\n Gas: {:.3f}\n Liquid: {:.3f}\n Solid: {:.3f}".format(
+    Y_gas, Y_liquid, Y_solid))
 
 
 ### Report the fractions within each phase (in OpenFOAM ready form)
 print("\nGas Fractions:")
 for specieName in Y_i_daf.keys():
-    Yspecie = Y_i_daf[specieName]
+    Yspecie = Y_i_gas[specieName]
     print(" {}: {:.3f}".format(
         specieName, Yspecie))
 
@@ -103,6 +121,8 @@ for specieName in sec_species.keys():
     sec_species[specieName] = Yspecie/sum(sec_species.values())
     print(" {}: {:.3f}".format(
         specieName, sec_species[specieName]))
+
+print("\n\n")
 
 
 
